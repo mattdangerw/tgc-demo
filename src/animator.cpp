39651965ad402caae05941134c471c9864ae2b19
Matthew Watson <mattdangerw@gmail.com>
2012-11-04 21:16:44 -0800

    Design changes to animator.

--- src/animator.cpp
+++ src/animator.cpp
@@ -4,7 +4,7 @@ Animation::Animation()
   : repeat_(false),
     finished_(false),
     forcing_(false),
-    stop_next_target_(false),
+    stop_next_keyframe_(false),
     next_keyframe_(0),
     time_(0.0f),
     lerp_t_(0.0f) {
@@ -16,6 +16,8 @@ Animation::~Animation() {}
 
 void Animation::addKeyframe(Keyframe key) {
   assert(key.time > 0);
+  // Add the keyframe in and keep things sorted.
+  // Obviously inefficient for many many keyframes, but that's not in the usage plan.
   for (vector<Keyframe>::iterator it = keyframes_.begin(); it != keyframes_.end(); ++it) {
     if ( key.time < it->time) {
       keyframes_.insert(it, key);
@@ -23,14 +25,14 @@ void Animation::addKeyframe(Keyframe key) {
   }
 }
 
-void Animation::start(int keyframe) {
-  last_keyframe_.index = keyframe;
+void Animation::start(int keyframe_index) {
+  last_keyframe_.index = keyframe_index;
   last_keyframe_.time = 0.0f;
   next_keyframe_ = 0;
   time_ = 0.0f;
   lerp_t_ = 0.0f;
   finished_ = keyframes_.empty();
-  stop_next_target_ = false;
+  stop_next_keyframe_ = false;
 }
 
 void Animation::startForced(int keyframe1, int keyframe2, float lerp_t) {
@@ -44,14 +46,16 @@ void Animation::update(float delta_time) {
   if (finished_) return;
   time_ += delta_time;
   while (time_ > keyframes_[next_keyframe_].time) {
+    // Move on to a next keyframe target.
     last_keyframe_ = keyframes_[next_keyframe_];
     ++next_keyframe_;
     forcing_ = false;
-    if (stop_next_target_) {
+    if (stop_next_keyframe_) {
       finished_ = true;
       return;
     }
     if (next_keyframe_ > keyframes_.size()) {
+      // We just finished. Start over if repeat is true.
       if (repeat_) {
         start(last_keyframe_.index);
         update(delta_time - last_keyframe_.time);
@@ -66,15 +70,18 @@ void Animation::update(float delta_time) {
 
 void Animation::currentKeyState(int keyframes[], float lerp_ts[]) {
   if (forcing_ == true) {
+    // We forced this animation to start in middle of another. So interpolting three different keyframes.
     keyframes[0] = last_keyframe_.index;
     keyframes[1] = last_keyframe_second_index_;
     keyframes[2] = keyframes_[next_keyframe_].index;
     lerp_ts[0] = last_keyframe_lerp_t_;
     lerp_ts[1] = lerp_t_;
   } else if (finished_) {
+    // Nothings really interpoling here, we just resting on the final keyframe of animation.
     keyframes[0] = keyframes[1] = keyframes[2] = last_keyframe_.index;
     lerp_ts[0] = lerp_ts[1] = 0.0f;
   } else {
+    // Interpolating between two keyframes in this animation.
     keyframes[0] = last_keyframe_.index;
     keyframes[1] = keyframes[2] = keyframes_[next_keyframe_].index;
     lerp_ts[0] = lerp_t_;
@@ -82,51 +89,63 @@ void Animation::currentKeyState(int keyframes[], float lerp_ts[]) {
   }
 }
 
-Animator::Animator()
-  : current_(NULL),
-    queued_(NULL),
-    start_keyframe_index_(0) {}
+Animator::Animator() : start_keyframe_index_(0) {}
 
 Animator::~Animator() {}
 
 void Animator::queueAnimation(string name) {
-  if (current_ != NULL) {
-    queued_ = &animations_[name];
-  } else {
-    current_ = &animations_[name];
-    current_->start(start_keyframe_index_);
+  Animation *next = &animations_[name];
+  if (queued_.empty()) {
+    // Nothings been animated yet. Start this one right away.
+    next->start(start_keyframe_index_);
+  } else if (queued_.front()->repeats()) {
+    // This animation repeats forever so we need to ask it to stop.
+    queued_.front()->stopAtNextKeyframe();
   }
+  queued_.push(next);
 }
 
 void Animator::forceAnimation(string name) {
-  if (current_ != NULL) {
-    // TODO: erase rest of queue.
-    if (current_->forcing()) {
-      queueAnimation(name);
-    } else {
-      Animation *next = &animations_[name];
-      next->startForced(current_->lastKeyframeIndex(), current_->nextKeyframeIndex(), current_->lerpT());
-      current_ = next;
-    }
+  Animation *next = &animations_[name];
+  if (queued_.empty()) {
+    // Nothings been animated yet. Start this one right away.
+    next->start(start_keyframe_index_);
   } else {
-    current_ = &animations_[name];
-    current_->start(start_keyframe_index_);
+    Animation *prev = queued_.front();
+    // Clear queue.
+    queued_ = queue<Animation *>();
+    if (prev->forcing()) {
+      // We can't force becuase the current animation is already using three keyframes to force.
+      // Add current animation back to queue and tell it to finish soon.
+      prev->stopAtNextKeyframe();
+      queued_.push(prev);
+    } else {
+      // Force this animation to start from two interpolated keyframes.
+      if (prev->finished()) {
+        next->start(prev->lastKeyframeIndex());
+      } else {
+        next->startForced(prev->lastKeyframeIndex(), prev->nextKeyframeIndex(), prev->lerpT());
+      }
+    }
   }
+  queued_.push(next);
 }
 
 void Animator::update(float delta_time) {
-  if (current_ != NULL) {
-    if (queued_ != NULL && current_->finished()) {
-      queued_->start(current_->lastKeyframeIndex());
-      current_ = queued_;
+  if (!queued_.empty()) {
+    if (queued_.size() > 1 && queued_.front()->finished()) {
+      // Move on to next animation.
+      Animation *last = queued_.front();
+      queued_.pop();
+      queued_.front()->start(last->lastKeyframeIndex());
     }
-    current_->update(delta_time);
+    queued_.front()->update(delta_time);
   }
 }
 
 void Animator::currentKeyState(int keyframes[], float lerp_ts[]) {
-  if (current_ != NULL) {
-    return current_->currentKeyState(keyframes, lerp_ts);
+  if (!queued_.empty()) {
+    return queued_.front()->currentKeyState(keyframes, lerp_ts);
   }
   keyframes[0] = keyframes[1] = keyframes[2] = start_keyframe_index_;
   lerp_ts[0] = lerp_ts[1] = 0.0f;
--- src/animator.h
+++ src/animator.h
@@ -23,22 +23,31 @@ class Animation {
     ~Animation();
     // Set up functions.
     void addKeyframe(Keyframe key);
-    void setRepeat(bool repeat) { repeat_ = repeat; }
+    // Repeat animation forever till asked to stop.
+    bool repeats() { return repeat_; }
+    void setRepeats(bool repeat) { repeat_ = repeat; }
+
     // Functions for during use (used by Animator).
-    void start(int keyframe);
+    // Start animation from the given keyframe.
+    void start(int keyframe_index);
+    // Start animation in the middle of another animation. So will briefly be interpolating between three keyframes.
     void startForced(int keyframe1, int keyframe2, float lerp_t);
+    void stopAtNextKeyframe() { stop_next_keyframe_ = true; }
+    void update(float delta_time);
+    // Is currently interpolating with three keyframes.
     bool forcing() { return forcing_; }
+    bool finished() { return finished_; }
+    // The last keyframe used in animation. If forcing there is no one keyframe, use currentKeyState.
     int lastKeyframeIndex() { return last_keyframe_.index; }
     int nextKeyframeIndex() { return keyframes_[next_keyframe_].index; }
     float lerpT() { return lerp_t_; }
-    void update(float delta_time);
-    void stopAtNextTarget() { stop_next_target_ = true; }
-    bool finished() { return finished_; }
+    // Gets the current keyframe state for use in rendering.
+    // Always gives three keyframes and two lerp values, no matter how many keyframes are actually used.
     void currentKeyState(int *keyframes, float *lerp_ts);
 
   private:
     vector<Keyframe> keyframes_;
-    bool repeat_, finished_, forcing_, stop_next_target_;
+    bool repeat_, finished_, forcing_, stop_next_keyframe_;
     Keyframe last_keyframe_;
     int next_keyframe_, last_keyframe_second_index_;
     float time_, lerp_t_, last_keyframe_lerp_t_;
@@ -51,16 +60,22 @@ class Animator {
     // Set up function.
     void setStartKeyframe(int index) { start_keyframe_index_ = index; }
     void addAnimation(string name, const Animation &animation) { animations_[name] = animation; }
+
     // Functions for during use.
+    // Queue up an animation. This will cause any repeating animations ahead in queue to stop.
     void queueAnimation(string name);
+    // Force start an animation. This will delete the queue and move this to front.
+    // If the current animation is between keyframes, this will treat that lerped target as the starting keyframe.
+    // Forcint the new animation to temporarily lerp between three keyframes.
     void forceAnimation(string name);
     void update(float delta_time);
+    // Gets the current keyframe state for use in rendering.
+    // Always gives three keyframes and two lerp values, no matter how many keyframes are actually used.
     void currentKeyState(int keyframes[], float lerp_ts[]);
 
   private:
     map<string, Animation> animations_;
-    Animation *current_;
-    Animation *queued_;
+    queue<Animation *> queued_;
     int start_keyframe_index_;
 };
 
